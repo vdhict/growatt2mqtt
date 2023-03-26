a0ed501b684139db2414f62212b85ef69ee783d2
Sander van der Heijden <sheijden@vdh-ict.nl>
2023-03-26 12:47:01 +0200

    changes to mapping

--- script/growattmon.py
+++ script/growattmon.py
@@ -3,14 +3,21 @@
 import time
 import os
 import paho.mqtt.client as mqtt
+import json
 
 from configparser import RawConfigParser
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.exceptions import ModbusIOException
 
 from growatt import Growatt
 
 settings = RawConfigParser()
 settings.read(os.path.dirname(os.path.realpath(__file__)) + '/solarmonitor.cfg')
+
+deviceregistersmapping = settings.get('mapping', 'devicemap')
+
+with open(os.path.dirname(os.path.realpath(__file__)) + "/mapping/" + deviceregistersmapping) as f:
+    mapping = json.load(f)
 
 interval = settings.getint('query', 'interval', fallback=1)
 offline_interval = settings.getint('query', 'offline_interval', fallback=60)
@@ -20,6 +27,15 @@ mqtt_host = settings.get('mqtt', 'host')
 mqtt_port = settings.getint('mqtt', 'port')
 mqtt_topic = settings.get('mqtt','topic', fallback = 'growatt')
 mqtt_clientid = settings.get('mqtt','clientid', fallback='inverter')
+
+# functions
+
+def get_single(registers, index, unit):
+    return round(float(registers[index]) * unit, 1)
+
+def get_double(registers, index, unit):
+    return round(float((registers[index] << 16) + registers[index+1])*unit, 1)
+
 
 # Clients
 
@@ -35,26 +51,33 @@ mqtt = mqtt.Client(mqtt_clientid)
 mqtt.on_connect = on_connect
 mqtt.connect(mqtt_host, mqtt_port)
 mqtt.loop_start()
-time.sleep(5)
 
-print('Setup Serial Connection... ', end='')
-port = settings.get('solarmon', 'port', fallback='/dev/ttyUSB0')
-client = ModbusClient(method='rtu', port=port, baudrate=9600, stopbits=1, parity='N', bytesize=8, timeout=1)
-client.connect()
-print('Done!')
+time.sleep(2)
+# Publish inverter and sensor configuration for HASS autodiscovery
 
-print('Loading inverter... ')
+mqtt.publish('test/homeassistant/sensor/growattmqtt/config',payload='{"name": "Growatt MQTT", "object_id": "growatt_inverter"}')
 
-unit = settings.get('inverter','unit', fallback='1')
-measurement = settings.get('inverter','measurement', fallback='measurement')
-growatt = Growatt(client, name, unit)
-growatt.print_info()
-inverter = {
-        'error_sleep': 0,
-        'growatt': growatt,
-        'measurement': measurement
-    }
-print('Done!')
+for reg in mapping['registerGroups']:
+    regMap = reg['registerMap']
+    for map in regMap:
+        topic = 'test/homeassistant/sensor/growatt_' + map + '/config'
+        payload='{"device_class" : "' + regMap[map]['class'] + '","name" : "'+ regMap[map]['name'] + '","state_topic" : "inverter/growattmqtt/'+map+'","unit_of_measurement" : "' + regMap[map]['unit']+'", "unique_id": "growatt_'+map+'", "object_id": "growatt_'+map+'", "device_id": "growatt_inverter"}'
+        mqtt.publish('test/homeassistant/sensor/growatt_' + map + '/config',payload='{"device_class" : "' + regMap[map]['class'] + '","name" : "'+ regMap[map]['name'] + '","state_topic" : "inverter/growattmqtt/'+map+'","unit_of_measurement" : "' + regMap[map]['unit']+'", "unique_id": "growatt_'+map+'", "object_id": "growatt_'+map+'", "device_id": "growatt_inverter"}', retain=True)
+
+
+
+# print('Setup Serial Connection... ', end='')
+# port = settings.get('solarmon', 'port', fallback='/dev/ttyUSB0')
+# client = ModbusClient(method='rtu', port=port, baudrate=9600, stopbits=1, parity='N', bytesize=8, timeout=1)
+# client.connect()
+# print('Done!')
+
+# print('Loading inverter... ')
+# unit = settings.get('inverter','unit', fallback='1')
+# print('Done!')
+
+
+
 
 
 while True:
